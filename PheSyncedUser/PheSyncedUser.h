c33528a2b8ff3ddcd2e0ba90fe644e6536f048a3
Mohamad Saeedi <mohamad.saeedi@bookatable.com>
2020-10-09 10:23:54 +0100

    Initial Commit

---
+++ PheSyncedUser/PheSyncedUser.h
@@ -0,0 +1,19 @@
+//
+//  PheSyncedUser.h
+//  PheSyncedUser
+//
+//  Created by Mohamad Saeedi on 09/10/2020.
+//  Copyright © 2020 Mohamad Saeedi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PheSyncedUser.
+FOUNDATION_EXPORT double PheSyncedUserVersionNumber;
+
+//! Project version string for PheSyncedUser.
+FOUNDATION_EXPORT const unsigned char PheSyncedUserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PheSyncedUser/PublicHeader.h>
+
+
