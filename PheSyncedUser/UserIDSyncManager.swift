e499e0ad8cc60cf98cb4e13812ef116f0281f2b4
Mohamad Saeedi <mohamad.saeedi@bookatable.com>
2020-10-30 14:26:38 +0000

    added secure store for better testability

--- PheSyncedUser/UserIDSyncManager.swift
+++ PheSyncedUser/UserIDSyncManager.swift
@@ -8,30 +8,30 @@
 import Foundation
 
 public protocol IDSyncManagerType {
+    // call syncId() before accessing userId
     var userId: String? { get }
 
     // generates id if does not exist and stores it in keychain otherwise returns existing
     func syncId() -> String?
 
     // clears id in keychain, only use if user wants to reset and remove their usage data
-    func clearId() -> OSStatus
+    func clearId()
 }
 
 public class UserIDSyncManager: IDSyncManagerType {
 
-    private let userIDKey = "userID"
-    private let defaultServiceId = "com.phe.securestore"
-    private let sharedAccessGroup = "6KQH7L94KJ.com.phe.sharedAccess"
-
     public private(set) var userId: String?
+    private let store: IDSecureStoreType
 
-    public init() { }
+    public init(with store: IDSecureStoreType = IDSecureStore()) {
+        self.store = store
+    }
 
     public func syncId() -> String? {
-        userId = retrieveId()
+        userId = store.retrieveId()
         if userId == nil {
             let generatedId = UUID().uuidString
-            let status = save(userId: generatedId)
+            let status = store.save(userId: generatedId)
             guard status == errSecSuccess else { return nil }
             debugPrint("generated and synced new user id")
             userId = generatedId
@@ -41,29 +41,36 @@ public class UserIDSyncManager: IDSyncManagerType {
         return userId
     }
 
-    public func clearId() -> OSStatus {
-        let query = [kSecClass: kSecClassGenericPassword,
-                     kSecAttrService: defaultServiceId,
-                     kSecAttrAccount: userIDKey,
-                     kSecAttrAccessGroup: sharedAccessGroup] as [String: Any]
-
-        return SecItemDelete(query as CFDictionary)
+    public func clearId() {
+        _ = store.clearId()
     }
 }
 
-private extension UserIDSyncManager {
-    private func save(userId: String) -> OSStatus {
-        let idData = userId.data(using: .utf8)
-        guard let id = idData else { return errSecInvalidData }
+public protocol IDSecureStoreType {
+    func save(userId: String) -> OSStatus
+    func retrieveId() -> String?
+    func clearId() -> OSStatus
+}
+
+public class IDSecureStore: IDSecureStoreType {
+
+    private let userIDKey = "userID"
+    private let defaultServiceId = "com.phe.securestore"
+    private let sharedAccessGroup = "6KQH7L94KJ.com.phe.sharedAccess"
+
+    public init() { }
+
+    public func save(userId: String) -> OSStatus {
+        guard let idData = userId.data(using: .utf8) else { return errSecInvalidData }
         let attributes = [kSecClass: kSecClassGenericPassword,
                           kSecAttrService: defaultServiceId,
                           kSecAttrAccount: userIDKey,
                           kSecAttrAccessGroup: sharedAccessGroup,
-                          kSecValueData: id] as [String: Any]
+                          kSecValueData: idData] as [String: Any]
         return SecItemAdd(attributes as CFDictionary, nil)
     }
 
-    private func retrieveId() -> String? {
+    public func retrieveId() -> String? {
         let query = [kSecClass: kSecClassGenericPassword,
                      kSecAttrService: defaultServiceId,
                      kSecAttrAccount: userIDKey,
@@ -73,5 +80,14 @@ private extension UserIDSyncManager {
         SecItemCopyMatching(query as CFDictionary, &item)
         guard let data = item as? Data else { return nil }
         return String(decoding: data, as: UTF8.self)
+    }
+
+    public func clearId() -> OSStatus {
+        let query = [kSecClass: kSecClassGenericPassword,
+                     kSecAttrService: defaultServiceId,
+                     kSecAttrAccount: userIDKey,
+                     kSecAttrAccessGroup: sharedAccessGroup] as [String: Any]
+
+        return SecItemDelete(query as CFDictionary)
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # PheSyncedUser
 Generate unique user Id and sync across PHE apps
+
+How to use
+
+1 - include PheSyncedUser in the podfile using: pod 'PheSyncedUser'
+2 - call pod install
+3 - Click on project name in project navigator then switch to Signing & Capabilities section
+4 - Add "Keychain Sharing" capability if it does not exist already by click "+ Capablility"
+5 - Add Keychain Access Group by clicking + sign at the bottom of Keychain Groups section
+6 - Enter "com.phe.sharedAccess" as the name for the access group
+
+7 - import PheSyncedUser into swift file 
+8 - call syncId() on UserIDSyncManager object before accessing userId
+
+        let idManager = UserIDSyncManager()
+        let userId = idManager.syncId()
+
+        also  print(idManager.userId)
